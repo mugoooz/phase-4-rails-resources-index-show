6bc1c0fcba8313e431d9ef698c17c595ff7e0eea
Eric Mugo <mugoericpg20@gmail.com>
2023-06-21 10:54:39 +0300

    made controller

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,5 @@
+class BirdsController < ApplicationController
+
+    
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/birds', to: 'birds#index'
-  get '/birds/:id', to: 'birds#show'
+  resources :birds, only: [:index, :show]
+
 
 end
